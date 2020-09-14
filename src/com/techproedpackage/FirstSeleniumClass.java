b3cee18602a9ea205e74ca94b570ca3f0f87a345
yasen911 <yasenarslan@gmail.com>
2020-09-15 00:10:58 +0200

    First proje

---
+++ src/com/techproedpackage/FirstSeleniumClass.java
@@ -0,0 +1,13 @@
+package com.techproedpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumClass {
+
+    public static void main(String[] args) {
+       System.setProperty("webdriver.chrome.driver","/Users/yasenarslan/Documents/Selenium Libraries/Drivers/chromedriver/");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+    }
+}
